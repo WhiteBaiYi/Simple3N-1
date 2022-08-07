365fceef3e48b8e9efcdcf5c92352cdda30228a6
Whiterely <78243160+WhiteBaiYi@users.noreply.github.com>
2022-08-07 22:35:36 +0800

    Add files via upload

--- 3N+1.cpp
+++ 3N+1.cpp
@@ -3,22 +3,20 @@ int main (){
 int n = 0,c = 0;
 	std::cout << "请输入一个数" << std::endl;
 	std::cin >> n ;
-	int con = 1;
-while (con){
+while (n != 1){
 	c = n % 2;
 if(c == 0) {
 	n /=2;
 	std::cout << "n= " << n << std::endl;
 	c = n % 2;
 	}
-if(c != 0)
+else
 	{n = 3*n+1;
 	std::cout << "n= " << n << std::endl;
-if (n == 4) {
-con = 0;
+	}
+if (n == 1) {
 std::cout << "此数满足3N+1\n";
 }
-	}
 	}
 return 0;
 }
